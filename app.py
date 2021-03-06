37e8f6607dfc2d708b17021792680f90f50a8fc9
ChristianJB77 <christian.bayerle@gmail.com>
2021-03-06 21:30:31 +0100

    Get user blog + remove starter todo app

--- app.py
+++ app.py
@@ -268,7 +268,17 @@ def create_app(test_config=None):
     """TRAVEL SECRETS BLOG"""
 
 
-    # View blog posts
+    # View blog posts USER VIEW
+    @app.route("/blog/user")
+    @requires_auth
+    def get_blog_user(jwt):
+        blogs = Secret.query.order_by(desc(Secret.id)).all()
+        userinfo=session[os.environ['PROFILE_KEY']]
+
+        return render_template("blog_user.html", blogs=blogs, userinfo=userinfo)
+
+
+    # View blog posts MASTER
     @app.route("/blog")
     @requires_auth_rbac('get:blog')
     def get_blog(jwt):
@@ -342,168 +352,9 @@ def create_app(test_config=None):
     def delete_blog_master(jwt, id):
         secret = Secret.query.filter(Secret.id == id).one_or_none()
         secret.delete()
+        flash("Blog was DELETED!")
 
         return jsonify({ 'success': True })
-
-
-    """TODOS"""
-
-
-    # Todo App main site / index
-    @app.route("/dashboard")
-    @requires_auth
-    def dashboard(jwt):
-        return redirect(url_for('get_todo_list', todos_list_id=1))
-
-    # New task
-    @app.route("/todos/create", methods=['POST'])
-    @requires_auth_rbac('post:task')
-    def create_todos(jwt):
-        #Default error is false
-        error = False
-        #Site content body dict to store data before database is closed
-        body = {}
-        try:
-            td = request.get_json()['todo_body']
-            list_id = request.get_json()['list_id_body']
-            todo = Todo(description=td, completed=False, todos_list_id=list_id)
-            db.session.add(todo)
-            db.session.commit()
-            body['id'] =  todo.id
-            body['completed'] = todo.completed
-            body['description'] = todo.description
-            body['todos_list_id'] = todo.todos_list_id
-        except:
-            #If error, rollback -> Don't insert in database
-            error = True
-            db.session.rollback()
-            print(sys.exc_info())
-        finally:
-            #Best practice to close database after operations
-            db.session.close()
-        if not error:
-            #If no error, then return body dict
-            return jsonify(body)
-
-
-    # Delete task
-    @app.route("/todos/<todo_id>/delete", methods=['DELETE'])
-    @requires_auth_rbac('delete:master')
-    def delete_todo(jwt, todo_id):
-        try:
-            Todo.query.filter_by(id=todo_id).delete()
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-        return jsonify({ 'success': True })
-
-
-    # Check completed task
-    @app.route("/todos/<todo_id>/set-completed", methods=['POST'])
-    @requires_auth_rbac('post:task-completed')
-    def completed_todo(jwt, todo_id):
-        try:
-            cd = request.get_json()['completed_body']
-            todo = Todo.query.get(todo_id)
-            todo.completed = cd
-            completed = Todo(completed=cd)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-        return redirect("/dashboard")
-
-
-    ## LISTS
-    # New list
-    @app.route("/lists/create", methods=['POST'])
-    @requires_auth_rbac('post:task')
-    def create_lists(jwt):
-        #Default error is false
-        error = False
-        #Site content body dict to store data before database is closed
-        body = {}
-        try:
-            ls = request.get_json()['list_body']
-            list = Todo_List(list_name=ls, completed=False)
-            db.session.add(list)
-            db.session.commit()
-            body['id'] =  list.id
-            body['completed'] = list.completed
-            body['list_name'] = list.list_name
-        except:
-            #If error, rollback -> Don't insert in database
-            error = True
-            db.session.rollback()
-            print(sys.exc_info())
-        finally:
-            #Best practice to close database after operations
-            db.session.close()
-        if not error:
-            #If no error, then return body dict
-            return jsonify(body)
-
-
-    # Delete list and its children"
-    @app.route("/lists/<list_id>/delete", methods=['DELETE'])
-    @requires_auth_rbac('delete:task')
-    def delete_list(jwt, list_id):
-        try:
-            list = Todo_List.query.get(list_id)
-            #Delete all todos of selected todos list by class relationship
-            for todo in list.todos:
-                db.session.delete(todo)
-
-            db.session.delete(list)
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-        return jsonify({ 'success': True })
-
-
-    # Check completed list and its children
-    @app.route("/lists/<list_id>/set-completed", methods=['POST'])
-    @requires_auth_rbac('post:task-completed')
-    def completed_list(jwt, list_id):
-        try:
-            list = Todo_List.query.get(list_id)
-            cd = request.get_json()['completed_body']
-            #Check all todos of selected todos list by class relationship
-            for todo in list.todos:
-                todo.completed = cd
-                completed = Todo(completed=cd)
-
-            list.completed = cd
-            completed = Todo_List(completed=cd)
-
-            db.session.commit()
-        except:
-            db.session.rollback()
-        finally:
-            db.session.close()
-        return redirect("/dashboard")
-
-
-    # Todo list view
-    @app.route('/lists/<todos_list_id>')
-    @requires_auth
-    def get_todo_list(jwt, todos_list_id):
-        todo_lists = Todo_List.query.order_by('id').all()
-        active_todo_list = Todo_List.query.get(todos_list_id)
-        todos = Todo.query.filter_by(todos_list_id=todos_list_id).order_by('id').all()
-        # Get Auth0 userinfo
-        userinfo=session[os.environ['PROFILE_KEY']]
-        userinfo_pretty = json.dumps(session[constants.JWT_PAYLOAD])
-
-        return render_template("dashboard.html", lists=todo_lists,
-                                active_todo_list=active_todo_list, data=todos,
-                                userinfo=userinfo,
-                                userinfo_pretty=userinfo_pretty)
 
 
     return app
